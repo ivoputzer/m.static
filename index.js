b8a012dd64b7680c25a507d3e21f33c6cd651234
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-06-09 17:23:07 +0200

    implements static server functionality

---
+++ index.js
@@ -0,0 +1,27 @@
+const encoding = 'utf8'
+
+const {createReadStream} = require('fs')
+const {createServer} = require('http')
+const {join, normalize} = require('path')
+
+function static(options){
+  if (!options) throw new Error('options are mandatory')
+  return createServer(requestHandlerFor(options))
+}
+
+function requestHandlerFor(options){
+  return (req, res) => {
+    const filename = join(options.cwd, normalize(req.url))
+    createReadStream(filename)
+      .on('error', errorHandlerFor(req, res))
+      .pipe(res)
+  }
+}
+
+function errorHandlerFor(req, res){
+  return (err) => {
+    res.end(JSON.stringify(err))
+  }
+}
+
+module.exports = {static}
