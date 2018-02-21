8af827ed01a035cce8707900943eb82df3c5f865
Christian Fei <crifei93@gmail.com>
2018-02-21 20:42:28 +0100

    use m.test

--- test/index.js
+++ test/index.js
@@ -2,36 +2,36 @@ const {static} = require('..')
 const {get} = require('http')
 const {equal, throws, notEqual} = require('assert')
 const {join} = require('path')
+const {test}= require('m.test')
 
-describe('m.icro', function () {
-  it('uses standard module loading', () => {
+test('m.icro', function () {
+  test('uses standard module loading', () => {
     let module = require('..')
     notEqual(module.static, undefined)
   })
 })
 
-describe('m.static', function(){
-  it('throws an error when no options are given', () => {
+test('m.static', function(){
+  test('throws an error when no options are given', () => {
     throws(static, Error)
   })
 
   const server = static({cwd: __dirname})
 
-  before((done) => server.listen(9999, done))
-  after((done) => server.close(done))
-
-  it('exposes a `listen` fn as of net.Server', () => {
+  test('exposes a `listen` fn as of net.Server', () => {
     equal(typeof server.listen, 'function')
   })
 
-  it('exposes a `close` fn as of net.Server', () => {
+  test('exposes a `close` fn as of net.Server', () => {
     equal(typeof server.close, 'function')
   })
 
-  it('loads static files within `cwd`', (done) => {
-    get('http://localhost:9999/index.html', (res) => {
-      equal(200, res.statusCode)
-      done()
+  test('loads static files within `cwd`', (done) => {
+    server.listen(9999, () => {
+      get('http://localhost:9999/index.html', (res) => {
+        equal(200, res.statusCode)
+        server.close(done)
+      })
     })
   })
 })
