50123c2adaa0daa0d4295a2ba531667b5d330c33
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-13 17:29:53 +0100

    removes for loop

--- test/lib/args.js
+++ test/lib/args.js
@@ -11,4 +11,14 @@ test('m.args', () => {
     const args = ['--port', '8080', '--cwd', '.']
     deepEqual(parse(args), {port: '8080', cwd: '.'})
   })
+
+  test('parses multiple flag ignoring not paired options', () => {
+    const args = ['--port', '8080', '--cwd', '.', '12']
+    deepEqual(parse(args), {port: '8080', cwd: '.'})
+  })
+
+  test('ignores items that not starts with --', () => {
+    const args = ['should', 'be', 'ignored', '--port', '8080', '--cwd', '.']
+    deepEqual(parse(args), {port: '8080', cwd: '.'})
+  })
 })
